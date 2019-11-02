acf8f6234de0d65845642ed429269449a66891cf
khk37601 <khk37601@gmail.com>
2019-11-02 11:48:12 +0900

    프로그래머스_자리수더하기

---
+++ SWEA/프로그래머스_자리수더하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    arr = list(map(int, list(str(n))))
+
+    # while n > 0:
+    #    tmp = n%10
+    #   n = n//10
+    #  answer +=tmp
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return sum(arr)
+
+
+
