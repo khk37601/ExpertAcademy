809f8d7abdaa2d03adca6af0e36b3661ea992af0
khk37601 <khk37601@gmail.com>
2019-12-24 00:08:16 +0900

    백준_파도반수열

---
+++ SWEA/백준_파도반수열.py
@@ -0,0 +1,12 @@
+
+
+n = int(input())
+
+for i in range(n):
+    s = int(input())
+    _list = [1, 1, 1]
+
+    for j in range(2,s):
+        _list.append(_list[j-2]+_list[j-1])
+
+    print(_list[s-1])
