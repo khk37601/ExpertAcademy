1814b8c52e6e5c8c3dda8bdf1c378ef82403c71d
khk37601 <khk37601@gmail.com>
2020-04-15 11:33:16 +0900

    백준_문자열 폭발

--- 문자열/문자열 폭발.py
+++ 문자열/문자열 폭발.py
@@ -12,21 +12,36 @@ string = sys.stdin.readline().strip()
 boom = sys.stdin.readline().strip()
 
 
-boom_length = len(boom)
-loop = 0
+stack = []
+is_ture = False
 
-while loop < len(string):
+for loop in string:
+    stack.append(loop)
 
-    if string[loop:(boom_length+loop)] == boom:
-       string = string[:loop]+string[(boom_length+loop):]
+    stack_len = len(stack)
+    boom_len = len(boom)
+    # 두개의 끝 문자가 같을 경우.
+    # python은 -1로 배열의 끝을 확인 할 수 있음
+    # 다른 언어로도 풀수 있는 방법으로 취함.
+    if stack[stack_len-1] == boom[boom_len-1]:
+        # stack 에 담겨져있는 제일 끝 문자열 비교 하기 위함.
+        index = stack_len - 1
+        # 뒤로 부터 검사하는 이유는 112ab  : 12ab를 맞취기 위함.
+        for loop2 in range(len(boom)-1, -1, -1):
+            #boom 문자와 일치하는지 확인.
+            if stack[index] == boom[loop2]:
+                is_ture = True
+                index -= 1
+            else:
+                is_ture = False
+                break
+        # 모든 문자가 일치 하는 경우
+        if is_ture:
+            # 문자열 크기 만큼 stack에서 뺴준다.
+            for inner_loop in boom:
+                stack.pop()
 
-       if loop-boom_length < 0:
-           loop = -1
-       else:
-           loop -= boom_length
-    loop +=1
-
-if string:
-    print(string)
+if stack:
+    print("".join(stack))
 else:
-    print("FLUA")
+    print("FRULA")
