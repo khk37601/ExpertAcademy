2a762166e2898024bf4c885fd94434cdc2b2eb42
khk37601 <khk37601@gmail.com>
2020-07-26 20:15:20 +0900

    Create 카카오_비밀지도.py

---
+++ 기타/카카오_비밀지도.py
@@ -0,0 +1,17 @@
+def full(bin, n):
+    return bin.zfill(n)
+
+
+def solution(n, arr1, arr2):
+    answer = []
+    for x, y in zip(arr1, arr2):
+        maze = ''
+        bin_x, bin_y = full(format(x, 'b'), n), full(format(y, 'b'), n)
+        for i, j in zip(bin_x, bin_y):
+            if int(i) or int(j) == 1:
+                maze += '#'
+            else:
+                maze += ' '
+        answer.append(maze)
+
+    return answer
